be9d38aaeef30dc920dfe7878c546cd47c406c10
Leonardo Messias <leonardo@wosi.com.br>
2025-10-07 20:37:59 -0300

    Remove o componente de cabeçalho das páginas de login e registro, substituindo-o por um logo e link de navegação, simplificando a estrutura do HTML e atualizando as importações no TypeScript.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
-import { HeaderComponent } from "../site/landing-page/header/header.component";
 
 @Component({
   selector: 'app-login',
-  imports: [HeaderComponent],
+  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
-import { HeaderComponent } from "../site/landing-page/header/header.component";
 
 @Component({
   selector: 'app-register',
-  imports: [HeaderComponent],
+  imports: [],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
